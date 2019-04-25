5ab95d5aab8a3ccde49b6821c6b0353337428005
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-04-24 23:42:57 -0300

    Modulo2: signup and avatar upload

--- Modulo2/src/routes.js
+++ Modulo2/src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const multerConfig = require('./config/multer')
+const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.send('Hello satanas')
-})
+const userController = require('./app/controllers/UserController')
+
+routes.get('/signup', userController.create)
+routes.post('/signup', upload.single('avatar'), userController.store)
 
 module.exports = routes
