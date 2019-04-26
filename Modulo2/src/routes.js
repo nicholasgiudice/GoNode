d8af73d86bbf97cbd6de9e212f3de709ae577f85
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-04-25 23:45:25 -0300

    Autentication and Appointments

--- Modulo2/src/routes.js
+++ Modulo2/src/routes.js
@@ -4,9 +4,34 @@ const upload = require('multer')(multerConfig)
 
 const routes = express.Router()
 
-const userController = require('./app/controllers/UserController')
+const authMiddleware = require('./app/middlewares/auth')
+const guestMiddleware = require('./app/middlewares/guest')
 
-routes.get('/signup', userController.create)
+const userController = require('./app/controllers/UserController')
+const sessionController = require('./app/controllers/SessionController')
+const dashboardController = require('./app/controllers/DashboardController')
+const fileController = require('./app/controllers/FileController')
+const appointmentController = require('./app/controllers/AppointmentController')
+
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success')
+  res.locals.flashError = req.flash('error')
+  return next()
+})
+
+routes.get('/', guestMiddleware, sessionController.create)
+routes.post('/signin', sessionController.store)
+routes.get('/signup', guestMiddleware, userController.create)
 routes.post('/signup', upload.single('avatar'), userController.store)
+
+routes.use('/app', authMiddleware)
+
+routes.get('/app/logout', sessionController.destroy)
+
+routes.get('/app/dashboard', dashboardController.index)
+
+routes.get('/files/:file', fileController.show)
+
+routes.get('/app/appointments/new/:provider', appointmentController.create)
 
 module.exports = routes
