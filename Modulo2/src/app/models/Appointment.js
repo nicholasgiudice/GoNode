a3e3b34c7f0b05df854d34a3ff31a259006120ff
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-04-27 16:45:24 -0300

    Fix Appointment Model

--- Modulo2/src/app/models/Appointment.js
+++ Modulo2/src/app/models/Appointment.js
@@ -1,9 +1,9 @@
 module.exports = (sequelize, DataTypes) => {
-  const Appointment = sequelize.define(' Appointment', {
+  const Appointment = sequelize.define('Appointment', {
     date: DataTypes.DATE
   })
 
-  Appointment.associete = models => {
+  Appointment.associate = models => {
     Appointment.belongsTo(models.User, { foreignKey: 'user_id' })
     Appointment.belongsTo(models.User, { foreignKey: 'provider_id' })
   }
