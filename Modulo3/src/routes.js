ad6e6cb82d7e517c6228ab7c12b2f4c35862d607
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-04-28 18:25:46 -0300

    jwt verify

--- Modulo3/src/routes.js
+++ Modulo3/src/routes.js
@@ -2,10 +2,15 @@ const express = require('express')
 
 const routes = express.Router()
 
+const authMiddleware = require('./app/middlewares/auth')
+
 const userController = require('./app/controllers/UserController')
 const sessionController = require('./app/controllers/SessionController')
 
 routes.post('/users', userController.store)
 routes.post('/session', sessionController.store)
+routes.get('/test', authMiddleware, (req, res) => {
+  return res.json('OK')
+})
 
 module.exports = routes
