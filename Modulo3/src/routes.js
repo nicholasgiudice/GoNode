7ec844c653cf83020476502ae7931df2be3c6fe9
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-04-28 18:05:10 -0300

    Auth with JWT

--- Modulo3/src/routes.js
+++ Modulo3/src/routes.js
@@ -3,7 +3,9 @@ const express = require('express')
 const routes = express.Router()
 
 const userController = require('./app/controllers/UserController')
+const sessionController = require('./app/controllers/SessionController')
 
 routes.post('/users', userController.store)
+routes.post('/session', sessionController.store)
 
 module.exports = routes
