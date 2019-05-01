f0747c3a2315fc74dd56f82cb97b075101781afd
Nicholas Giudice <nicholas.giudice@outlook.com>
2019-05-01 16:09:58 -0300

    Modulo3: Queue e Sentry

--- Modulo3/src/config/mail.js
+++ Modulo3/src/config/mail.js
@@ -1,9 +1,9 @@
 module.exports = {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'cccceb13d43739',
-    pass: '65df00158a5dfd'
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   }
 }
